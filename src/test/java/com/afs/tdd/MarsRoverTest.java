808f42748a5dd3512776866901577fa156298fa7
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-15 17:09:14 +0800

    [MarsRover][baron] test: add base test case for single M movement

---
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -0,0 +1,24 @@
+package com.afs.tdd;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MarsRoverTest {
+
+    @Test
+    void should_x0_y1_directionN_when_execute_given_x0_y0_directionN_command_M() {
+        int x = 0;
+        int y = 0;
+        String direction = "N";
+
+        int expectedX = 0;
+        int expectedY = 1;
+        String expectedDirection = "N";
+
+        MarsRover rover = new MarsRover(x, y, direction);
+
+        Assertions.assertEquals(expectedX, rover.getCoordX());
+        Assertions.assertEquals(expectedY, rover.getCoordY());
+        Assertions.assertEquals(expectedDirection, rover.getDirection());
+    }
+}
