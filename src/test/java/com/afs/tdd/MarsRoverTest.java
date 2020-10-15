e5c277faa2a48794628eae7eef3d29219bd40ac2
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-15 21:37:56 +0800

    [MarsRover][baron] test: add test cases for x=0 y=0 direction=EAST

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -142,4 +142,75 @@ class MarsRoverTest {
         Assertions.assertEquals(expectedY, rover.getCoordY());
         Assertions.assertEquals(expectedDirection, rover.getDirection());
     }
+
+    /*
+        initial x = 0, y = 0, direction = EAST
+     */
+    @Test
+    void should_x1_y0_directionEAST_when_execute_given_x0_y0_directionEAST_command_MOVE() {
+        // given
+        int x = 0;
+        int y = 0;
+        String direction = MOVEMENT_KEYWORDS.EAST;
+
+        String commands = MOVEMENT_KEYWORDS.MOVE_KEY;
+        int expectedX = 1;
+        int expectedY = 0;
+        String expectedDirection = MOVEMENT_KEYWORDS.EAST;
+
+        // when
+        MarsRover rover = new MarsRover(x, y, direction);
+        rover.performCommands(commands);
+
+        // then
+        Assertions.assertEquals(expectedX, rover.getCoordX());
+        Assertions.assertEquals(expectedY, rover.getCoordY());
+        Assertions.assertEquals(expectedDirection, rover.getDirection());
+    }
+
+    @Test
+    void should_x0_y0_directionNORTH_when_execute_given_x0_y0_directionEAST_command_LEFT() {
+        // given
+        int x = 0;
+        int y = 0;
+        String direction = MOVEMENT_KEYWORDS.EAST;
+
+        String commands = MOVEMENT_KEYWORDS.TURN_LEFT_KEY;
+        int expectedX = 0;
+        int expectedY = 0;
+        String expectedDirection = MOVEMENT_KEYWORDS.NORTH;
+
+        // when
+        MarsRover rover = new MarsRover(x, y, direction);
+        rover.performCommands(commands);
+
+        // then
+        Assertions.assertEquals(expectedX, rover.getCoordX());
+        Assertions.assertEquals(expectedY, rover.getCoordY());
+        Assertions.assertEquals(expectedDirection, rover.getDirection());
+    }
+
+    @Test
+    void should_x0_y0_directionSOUTH_when_execute_given_x0_y0_directionEAST_command_RIGHT() {
+        // given
+        int x = 0;
+        int y = 0;
+        String direction = MOVEMENT_KEYWORDS.EAST;
+
+        String commands = MOVEMENT_KEYWORDS.TURN_RIGHT_KEY;
+        int expectedX = 0;
+        int expectedY = 0;
+        String expectedDirection = MOVEMENT_KEYWORDS.SOUTH;
+
+        // when
+        MarsRover rover = new MarsRover(x, y, direction);
+        rover.performCommands(commands);
+
+        // then
+        Assertions.assertEquals(expectedX, rover.getCoordX());
+        Assertions.assertEquals(expectedY, rover.getCoordY());
+        Assertions.assertEquals(expectedDirection, rover.getDirection());
+    }
+
+
 }
