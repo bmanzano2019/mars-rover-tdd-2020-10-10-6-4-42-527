c446566be83154345fb11ba13064c792872e5205
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-15 20:36:26 +0800

    [MarsRover][baron] test: add test cases for scenario x=0 y=0 direction=NORTH

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -5,22 +5,82 @@ import org.junit.jupiter.api.Test;
 
 class MarsRoverTest {
 
+    private static final String NORTH = "N";
+    private static final String SOUTH = "S";
+    private static final String EAST = "E";
+    private static final String WEST = "W";
+
+    private static final String MOVE_KEY = "M";
+    private static final String TURN_LEFT_KEY = "L";
+    private static final String TURN_RIGHT_KEY = "R";
+
+    /*
+        initial x = 0, y = 0, direction = N
+     */
     @Test
-    void should_x0_y1_directionN_when_execute_given_x0_y0_directionN_command_M() {
+    void should_x0_y1_directionNORTH_when_execute_given_x0_y0_directionNORTH_command_MOVE() {
+        // given
         int x = 0;
         int y = 0;
-        String direction = "N";
-        String commands = "M";
+        String direction = NORTH;
 
+        String commands = MOVE_KEY;
         int expectedX = 0;
         int expectedY = 1;
-        String expectedDirection = "N";
+        String expectedDirection = NORTH;
 
+        // when
         MarsRover rover = new MarsRover(x, y, direction);
         rover.performCommands(commands);
 
+        // then
         Assertions.assertEquals(expectedX, rover.getCoordX());
         Assertions.assertEquals(expectedY, rover.getCoordY());
         Assertions.assertEquals(expectedDirection, rover.getDirection());
     }
+
+    @Test
+    void should_x0_y0_directionWEST_when_execute_given_x0_y0_directionNORTH_command_LEFT() {
+        // given
+        int x = 0;
+        int y = 0;
+        String direction = NORTH;
+
+        String commands = TURN_LEFT_KEY;
+        int expectedX = 0;
+        int expectedY = 0;
+        String expectedDirection = WEST;
+
+        // when
+        MarsRover rover = new MarsRover(x, y, direction);
+        rover.performCommands(commands);
+
+        // then
+        Assertions.assertEquals(expectedX, rover.getCoordX());
+        Assertions.assertEquals(expectedY, rover.getCoordY());
+        Assertions.assertEquals(expectedDirection, rover.getDirection());
+    }
+
+    @Test
+    void should_x0_y0_directionEAST_when_execute_given_x0_y0_directionNORTH_command_RIGHT() {
+        // given
+        int x = 0;
+        int y = 0;
+        String direction = NORTH;
+
+        String commands = TURN_LEFT_KEY;
+        int expectedX = 0;
+        int expectedY = 0;
+        String expectedDirection = EAST;
+
+        // when
+        MarsRover rover = new MarsRover(x, y, direction);
+        rover.performCommands(commands);
+
+        // then
+        Assertions.assertEquals(expectedX, rover.getCoordX());
+        Assertions.assertEquals(expectedY, rover.getCoordY());
+        Assertions.assertEquals(expectedDirection, rover.getDirection());
+    }
+
 }
